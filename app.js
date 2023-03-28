346815ae94cf3fe1f0f53ebeab05a8a6cd05a055
cgibbs-snyk <108678176+cgibbs-snyk@users.noreply.github.com>
2023-03-28 15:49:31 +0100

    test remove hardcoded

--- app.js
+++ app.js
@@ -71,6 +71,7 @@ if (app.get('env') == 'development') {
   app.use(errorHandler());
 }
 
+// deepcode ignore HardcodedNonCryptoSecret: Test123
 var token = 'SECRET_TOKEN_f8ed84e8f41e4146403dd4a6bbcea5e418d23a9';
 console.log('token: ' + token);
 
